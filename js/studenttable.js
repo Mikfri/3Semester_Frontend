59ef3f7ecf83f9baaeee9534210a63ac0bfa4654
V1uff69 <kenn622r@edu.zealand.dk>
2023-12-01 03:25:29 +0100

    funktionality and styling

--- js/studenttable.js
+++ js/studenttable.js
@@ -1,7 +1,13 @@
 // js/table.js
-
+var totalStudents = 0;
 var hereTodayCounter = 0; // Counter for "Here Today"
 var notHereTodayCounter = 0; // Counter for "Not Here Today"
+
+// Get the current date and format it as "dd-mm-yy"
+const formattedDate = new Date().toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit' });
+
+// Display the formatted date in the HTML element with the id "todaysDate"
+document.getElementById('todaysDate').textContent = formattedDate;
 
 // Initialize counters on page load
 updateCounters();
@@ -31,9 +37,11 @@ function filterTable(filterType) {
 }
 
 function updateCounters() {
+  totalStudents = countStudents(); // Remove 'totalStudents' argument
   hereTodayCounter = countStudents('hereToday');
   notHereTodayCounter = countStudents('notHereToday');
 
+  updateCounter('totalStudents', totalStudents);
   updateCounter('hereToday', hereTodayCounter);
   updateCounter('notHereToday', notHereTodayCounter);
 }
@@ -45,12 +53,13 @@ function countStudents(counterType) {
   tableRows.forEach(function (row) {
     var checkInTime = row.children[2].textContent;
 
-    if (counterType === 'hereToday' && checkInTime) {
-      count++;
-    } else if (counterType === 'notHereToday' && !checkInTime) {
+    if (!counterType || (counterType === 'hereToday' && checkInTime) || (counterType === 'notHereToday' && !checkInTime)) {
       count++;
     }
   });
+
+  // Update the totalStudents variable
+  totalStudents = count;
 
   return count;
 }
