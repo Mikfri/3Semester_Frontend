9041be61359861bbe0ae7ad43c6edd7ac22d125c
Mikfri <mikk908w@edu.zealand.dk>
2023-12-12 22:27:20 +0100

    CleanUp

--- js/main.js
+++ js/main.js
@@ -14,15 +14,7 @@ const app = Vue.createApp({
                 const response = await axios.get(apiUrl);
                 const studentData = response.data;
 
-                // Mappe Unix Time-stempler til datoer
-                const formattedData = studentData.map(student => {
-                    return {
-                        ...student,
-                        timeArrived: new Date(student.timeArrived * 1000),
-                        timeLeft: new Date(student.timeLeft * 1000),
-                        timeCreated: new Date(student.timeCreated * 1000)
-                    };
-                });
+                const formattedData = this.convertUnixTimeToDates(studentData);
 
                 // Grupper data efter hver arbejdsdag (mandag til fredag)
                 const groupedData = {
@@ -48,6 +40,17 @@ const app = Vue.createApp({
                 console.error('Error fetching data:', error);
             }
         },
+        // Denne funktion sørger for brugeren får vist ´Invalid Date´ hvis Time Unix værdien er 0, for timeArrived og timeLeft
+        convertUnixTimeToDates(studentData) {   
+            return studentData.map(student => {
+                return {
+                    ...student,
+                    timeArrived: student.timeArrived === 0 ? 'Student has not arrived' : new Date(student.timeArrived * 1000),
+                    timeLeft: student.timeLeft === 0 ? 'Student has not checked out' : new Date(student.timeLeft * 1000),
+                    timeCreated: new Date(student.timeCreated * 1000)
+                };
+            });
+        },
         formatDate(date) {
             return new Date(date).toLocaleDateString('en-GB', {
                 weekday: 'short',
@@ -57,42 +60,34 @@ const app = Vue.createApp({
                 hour: '2-digit',
                 minute: '2-digit'
             })
-        },
-        // displayTimeArrived(timeArrived) {
-        //     return timeArrived === 0 ? 'Student has not arrived' : formatDate(timeArrived);
-        // },
+        },       
+        // filterByWeekNo(weekNo) {
+        //     const selectedDate = new Date(weekNo + '-1'); // Get date for the selected week's Monday
+        //     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-        // displayTimeLeft(timeLeft) {
-        //     return timeLeft === 0 ? 'Student has not checked out' : formatDate(timeLeft);
-        // },
-        filterByWeekNo(weekNo) {
-            const selectedDate = new Date(weekNo + '-1'); // Get date for the selected week's Monday
-            const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        //     const filteredData = {};
+        //     for (const day of days) {
+        //         filteredData[day] = this.studentData[day].filter(student => {
+        //             const studentDate = new Date(student.timeArrived);
+        //             const startOfWeek = new Date(selectedDate);
+        //             const endOfWeek = new Date(selectedDate);
+        //             endOfWeek.setDate(selectedDate.getDate() + 6);
 
-            const filteredData = {};
-            for (const day of days) {
-                filteredData[day] = this.studentData[day].filter(student => {
-                    const studentDate = new Date(student.timeArrived);
-                    const startOfWeek = new Date(selectedDate);
-                    const endOfWeek = new Date(selectedDate);
-                    endOfWeek.setDate(selectedDate.getDate() + 6);
+        //             return studentDate >= startOfWeek && studentDate <= endOfWeek;
+        //         });
+        //     }
 
-                    return studentDate >= startOfWeek && studentDate <= endOfWeek;
-                });
-            }
-
-            return filteredData;
-        }
+        //     return filteredData;
+        // }
     },
-    watch: {
-        selectedWeekNo(newValue) {
-            this.fetchData(newValue); // Kalder fetchData med det nye ugenummer
-        }
-    },
+    // watch: {
+    //     selectedWeekNo(newValue) {
+    //         this.fetchData(newValue); // Kalder fetchData med det nye ugenummer
+    //     }
+    // },
     mounted() {
         this.fetchData();
     }
 });
 
 app.mount('#app');
-
