5bf07190ea131d72e326d7129c8f98afd53ea7e6
Mikfri <mikk908w@edu.zealand.dk>
2023-12-12 22:30:22 +0100

    Plez

--- js/main.js
+++ js/main.js
@@ -3,45 +3,26 @@ const apiUrl = 'https://zealandconnect.azurewebsites.net/api/Activitydata';
 const app = Vue.createApp({
     data() {
         return {
-            studentData: {}, // Gemmer de hentede data
+            studentData: {},
             selectedWeekNo: '',
-            selectedWeekday: '' // Standard valgt arbejdsdag
+            selectedWeekday: '',
+            weekdays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
         };
     },
     methods: {
-        async fetchData(weekNo) {
+        async fetchData() {
             try {
                 const response = await axios.get(apiUrl);
                 const studentData = response.data;
 
                 const formattedData = this.convertUnixTimeToDates(studentData);
 
-                // Grupper data efter hver arbejdsdag (mandag til fredag)
-                const groupedData = {
-                    Monday: [],
-                    Tuesday: [],
-                    Wednesday: [],
-                    Thursday: [],
-                    Friday: [],
-                };
-
-                formattedData.forEach(student => {
-                    const dayOfWeek = student.timeCreated.getDay();
-                    const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
-
-                    // Tildel hver student til deres respektive arbejdsdag
-                    if (dayName in groupedData) {
-                        groupedData[dayName].push(student);
-                    }
-                });
-
-                this.studentData = groupedData; // Opdater Vue data med grupperet data efter arbejdsdag
+                this.groupDataByWeekdays(formattedData);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         },
-        // Denne funktion sørger for brugeren får vist ´Invalid Date´ hvis Time Unix værdien er 0, for timeArrived og timeLeft
-        convertUnixTimeToDates(studentData) {   
+        convertUnixTimeToDates(studentData) {
             return studentData.map(student => {
                 return {
                     ...student,
@@ -51,6 +32,25 @@ const app = Vue.createApp({
                 };
             });
         },
+        groupDataByWeekdays(formattedData) {
+            const groupedData = {
+                Monday: [],
+                Tuesday: [],
+                Wednesday: [],
+                Thursday: [],
+                Friday: [],
+            };
+
+            formattedData.forEach(student => {
+                const dayOfWeek = student.timeCreated.getDay();
+                if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+                    const dayName = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'][dayOfWeek - 1];
+                    groupedData[dayName].push(student);
+                }
+            });
+
+            this.studentData = groupedData;
+        },
         formatDate(date) {
             return new Date(date).toLocaleDateString('en-GB', {
                 weekday: 'short',
@@ -59,32 +59,9 @@ const app = Vue.createApp({
                 year: '2-digit',
                 hour: '2-digit',
                 minute: '2-digit'
-            })
-        },       
-        // filterByWeekNo(weekNo) {
-        //     const selectedDate = new Date(weekNo + '-1'); // Get date for the selected week's Monday
-        //     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-        //     const filteredData = {};
-        //     for (const day of days) {
-        //         filteredData[day] = this.studentData[day].filter(student => {
-        //             const studentDate = new Date(student.timeArrived);
-        //             const startOfWeek = new Date(selectedDate);
-        //             const endOfWeek = new Date(selectedDate);
-        //             endOfWeek.setDate(selectedDate.getDate() + 6);
-
-        //             return studentDate >= startOfWeek && studentDate <= endOfWeek;
-        //         });
-        //     }
-
-        //     return filteredData;
-        // }
+            });
+        },
     },
-    // watch: {
-    //     selectedWeekNo(newValue) {
-    //         this.fetchData(newValue); // Kalder fetchData med det nye ugenummer
-    //     }
-    // },
     mounted() {
         this.fetchData();
     }
